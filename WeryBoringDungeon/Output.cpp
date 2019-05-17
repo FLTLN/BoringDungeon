4b0626a806c6c893d956a589fc2437026f14cbff
Andrei Zhuravlev <dwasdx@gmail.com>
2019-05-17 20:38:57 +0300

    Вроде сделал через кучу циклов и условий, скорее всего можно намного лучше. Не тестил!!!! Есть вопросы с сохранением массива в памяти

--- WeryBoringDungeon/Output.cpp
+++ WeryBoringDungeon/Output.cpp
@@ -4,36 +4,8 @@
 char* makeMas(char* par, int w, int h, int x, int y)
 {
 	static char mas[9][9];
-	int XdifTOP, XdifBOT, YdifLEFT,YdifRIGHT;
-	XdifTOP = x - 4;
-	YdifLEFT = y - 4;
-	XdifBOT = x + 4;
-	YdifRIGHT = y + 4;
-	//if (XdifTOP < 0)
-	//{
-	//	if (YdifLEFT < 0)
-	//	{
-	//		//int a = -XdifTOP;
-	//		YdifLEFT = -YdifLEFT;
-	//		for (int i = 0, a = -XdifTOP; i < a; i++)
-	//		{
-	//			for (int j = 0; j < YdifLEFT; j++)
-	//			{
-	//				mas[i][j] = '#';
-	//			}
-	//		}
-	//	}
-	//	else if (YdifRIGHT > w)
-	//	{
-	//		for (int i = 0, a = -XdifTOP; i < a; i++)
-	//		{
-	//			for (int j = 0; j < YdifLEFT; j++)
-	//			{
-	//				mas[i][j] = '#';
-	//			}
-	//		}
-	//	}
-	//}
+	int a = 0, b = 0;
+	int T = 0, B = 0, L = 0, R = 0;
 	if (x < 4)
 	{
 		for (int i = 0; i < 9; i++)
@@ -41,41 +13,112 @@ char* makeMas(char* par, int w, int h, int x, int y)
 			for (int j = 0; j < -(x - 4); j++)
 			{
 				mas[i][j] = '#';
+				a--;
+				L++;
 			}
 		}
+
+		if (y < 4)
+		{
+			for (int i = 0; i < -(y - 4); i++)
+			{
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = '#';
+				b--;
+				T++;
+			}
+			for (int i = T, d = -40; i < 9; i++, d += 9 + L)		
+				for (int j = L; j < 9; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}
+		else if (y + 4 > h - 1)
+		{
+			for (int i = 9 - (h - y - 1); i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = '#';
+				b++;
+				B++;
+			}
+			for (int i = 0, d = -40; i < 9 - B; i++, d += 9 + L)
+				for (int j = L; j < 9; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}	
 	}
-	else if (x + 4 > w)
+	else if (x + 4 > w - 1)
 	{
 		for (int i = 0; i < 9; i++)
 		{
 			for (int j = 9 - (w - x - 1); j < 9; j++)
 			{
 				mas[i][j] = '#';
+				a++;
+				R++;
 			}
 		}
-	}
-	if (y < 4)
-	{
-		for (int i = 0; i < -(y - 4); i++)
-		{
-			for (int j = 0; j < 9; j++)
-			{
-				mas[i][j] = '#';
-			}
-		}
-	}
-	else if (y + 4 > h)
-	{
-		for (int i = 9 - (h - y - 1); i < 9; i++)
-		{
-			for (int j = 0; j < 9; j++)
-			{
-				mas[i][j] = '#';
-			}
-		}
-	}
-	//закончили с исключениями
 
-	mas[4][4] = par[(y * w + x)];		//надо будет про математику побазарить
+		if (y < 4)
+		{
+			for (int i = 0; i < -(y - 4); i++)
+			{
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = '#';
+				b--;
+				T++;
+			}
+			for (int i = T, d = -40; i < 9; i++, d += 9 + L)
+				for (int j = 0; j < 9 - R; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}
+		else if (y + 4 > h)
+		{
+			for (int i = 9 - (h - y - 1); i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = '#';
+				b++;
+				B++;
+			}
+			for (int i = 0, d = -40; i < 9 - B; i++, d += 9 + L)
+				for (int j = 0; j < 9 - R; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}
+	}
+	else
+	{
+		if (y < 4)
+		{
+			for (int i = 0; i < -(y - 4); i++)
+			{
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = '#';
+				b--;
+			}
+			for (int i = T, d = -40; i < 9; i++, d += 9 + L)
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}
+		else if (y + 4 > h)
+		{
+			for (int i = 9 - (h - y - 1); i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = '#';
+				b++;
+			}
+			for (int i = 0, d = -40; i < 9 - B; i++, d += 9 + L)
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}
+		else
+		{
+			for (int i = 0, d = -40; i < 9; i++, d += 9)			//запись лабиринта в массив в общем случае
+				for (int j = 0; j < 9; j++)
+					mas[i][j] = par[((y * w + x) + d + j)];
+		}
+	}
+	//надо будет про математику побазарить
 
 }
+
+
