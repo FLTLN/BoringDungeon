03bfa9fd29141177841078a8e977147f812282c3
Максим Захаров <mttpr28@gmail.com>
2019-06-27 23:31:05 +0300

    добавил парочку плюшек к функциям персонажа

--- WeryBoringDungeon/struk.cpp
+++ WeryBoringDungeon/struk.cpp
@@ -1,5 +1,6 @@
 #include "struk.h"
 #include <stdio.h>
+#include <malloc.h>
 
 
 void right(CHARACTER* current_location) {
@@ -25,3 +26,18 @@ void boring_change(CHARACTER* boring_val){															//изменение с
 void boring_change_event(CHARACTER* boring_val, int change) {									   //изменение скуки при событии
 	boring_val->boring_par->value += change;
 }
+
+CHARACTER * character_init()
+{
+	CHARACTER * new_character = (CHARACTER *)malloc(sizeof(CHARACTER));
+	new_character->boring_par = (BORING *)malloc(sizeof(BORING));
+	new_character->koord_char = (COORDINATES *)malloc(sizeof(COORDINATES));
+
+	return new_character;
+};
+
+int is_bored(CHARACTER * character)
+{
+	if (character->boring_par->value <= 0) return 0;
+	else return 1;
+};
