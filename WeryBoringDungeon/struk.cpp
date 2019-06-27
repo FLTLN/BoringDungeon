bb564dba91fcd3ca5d8f414441cbee1bd4299172
Светлана Щиколодкина <50289184+Schss@users.noreply.github.com>
2019-06-27 11:47:59 +0300

    функция "скуки" персонажа

--- WeryBoringDungeon/struk.cpp
+++ WeryBoringDungeon/struk.cpp
@@ -12,3 +12,16 @@ void left(CHARACTER* current_location) {
 
 void down(CHARACTER* current_location) {
 	current_location->koord_char->y++;
+}
+
+void up(CHARACTER* current_location) {
+	current_location->koord_char->y = current_location->koord_char->y-1;
+}
+
+void boring_change(CHARACTER* boring_val){															//изменение скуки в течение игры
+	boring_val->boring_par->value = boring_val->boring_par->value+ boring_val->boring_par->change;
+}
+
+void boring_change_event(CHARACTER* boring_val, int change) {									   //изменение скуки при событии
+	boring_val->boring_par->value = boring_val->boring_par->value+change;
+}
